95fc192f977038e9e0c2e58261107be2980065d6
sera113 <selaph1122@gmail.com>
2026-06-21 02:27:27 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const config = {
-    localStoragePrefix: "party-rank-sorter",
-    title: "Party Rank Sorter",
+    localStoragePrefix: "yozuca",
+    title: "yozuca",
     description: "Party rank sorter for your custom list of songs."
 };
